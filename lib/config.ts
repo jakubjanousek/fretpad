6a13ad7d743b499c84a7c4d618829d61ff39ea31
jakubjanousek <jakub.janousek@gmail.com>
2026-03-14 14:37:10 +0100

    Fix failing build

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,2 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
-
-if (process.env.NODE_ENV === "production" && !siteUrl) {
-  throw new Error("NEXT_PUBLIC_SITE_URL must be set in production.");
-}
-
-export const SITE_URL = siteUrl || "http://localhost:3000";
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL || "https://fretpad.com";
